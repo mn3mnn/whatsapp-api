bdbe8a5df5347250a9082db766683ad23afd64d7
Monaem Tarek <11410120210220@stud.cu.edu.eg>
2024-01-16 00:39:24 +0200

    package.json and sendMessage.js

--- sendMessages.js
+++ sendMessages.js
@@ -2,9 +2,9 @@ const { default: axios } = require("axios");
 const { globalApiKey } = require("./config");
 
 for (let i = 0; i < 10; i++) {    
-    axios.post('http://localhost:3000/api/send-message', {
+    axios.post('http://20.203.97.197/api/send-message', {
         "content": `Hello world ${i}`,
-        "mobile_number": "201129861466",
+        "mobile_number": "201093438380",
         "key": globalApiKey
     })
     .then((res) => {
@@ -14,3 +14,18 @@ for (let i = 0; i < 10; i++) {
         console.error(error)
     })
 }
+
+
+// for (let i = 0; i < 10; i++) {    
+//     axios.post('http://localhost:3000/api/send-message', {
+//         "content": `Hello world ${i}`,
+//         "mobile_number": "201129861466",
+//         "key": globalApiKey
+//     })
+//     .then((res) => {
+//         console.log(res)
+//     })
+//     .catch((error) => {
+//         console.error(error)
+//     })
+// }
